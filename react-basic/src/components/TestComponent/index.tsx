2b446f19521faf7f77b31b9fdc91f54c33ec8835
newstar <maori36@naver.com>
2023-07-07 17:23:02 +0900

    23.07.07

--- react-basic/src/components/TestComponent/index.tsx
+++ react-basic/src/components/TestComponent/index.tsx
@@ -4,17 +4,27 @@
 // 관리를 용이하게 해줌
 // class 컴포넌트 / function 컴포넌트
 
-function TestComponent () {
+// 컴포넌트 선언시 주의사항
+// 컴포넌트명의 첫 글자는 반드시 '대문자'이어야 함 
+function TestComponent () { 
+
+    const itemList = ['첫번째', '두번째', '세번쨰', '네번째', '다섯번째'];
+    const flag = true;
 
     // return 위치에 화면에 출력할 HTML 작성
     // 컴포넌트 return 시 주의사항
     // 반드시 하나의 요소만 반환
-    return (
+
+    // return에 작성하는 HTML 내부에서 javascript를 사용하고 싶으면
+    // {} 내부에 작성함 
+    // 그 {} 내부에서 다시 HTML을 반환하고 싶으면 () 내부에 작성 // 주석도 javascript형식으로 { /* */ }
+    return ( //! return위치에 HTML 작성해야하니까 map배열 함수로 선언 
         <div>
-        <div>첫번째 컴포넌트</div>
-        <div>두번째 컴포넌트</div>
-        <div>세번째 컴포넌트</div>
-        <div>네번째 컴포넌트</div>
+            {/* 주석 */}
+            { itemList.map( (item) => (<div>{item}컴포넌트입니다.</div>) ) }
+            {
+                flag ? (<div>TRUE</div>) : (<div>FALSE</div>)
+            }
         </div>
     );
 };
