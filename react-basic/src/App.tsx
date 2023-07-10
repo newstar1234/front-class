d6f10b6a4cf630a7fddfee2706e2d1aeac439868
newstar <maori36@naver.com>
2023-07-10 17:32:19 +0900

    23.07.10

--- react-basic/src/App.tsx
+++ react-basic/src/App.tsx
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
+export default function App() {
+  return (
+    <div>App</div>
+  )
 }
-
-export default App
--- react-basic/src/App2.tsx
+++ react-basic/src/App2.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import './style.css'; 
-// import TestComponent, { testVariable } from './components/TestComponent';
+import TestComponent, { testVariable } from './components/TestComponent';
 import Header from './views/layout/Header';
 import Main from './views/layout/Main';
 import Footer from './views/layout/Footer';
